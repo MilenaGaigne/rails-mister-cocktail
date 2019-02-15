e728038890ad95700c7711b22ea273deced1aaa0
Milena Gaigne <milena.gaigne@gmail.com>
2019-02-15 11:40:13 +0100

    ok

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,33 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params_cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(:cocktail_id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+  private
+
+  def params_cocktail
+  params.require(:cocktail).permit(:name)
+  end
+end
